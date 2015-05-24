68ced7987d3146b253f73e180e0095c3d66a628a
khiraiwa <the.world.nova@gmail.com>
2015-05-24 17:10:58 +0900

    [WIP] test

--- test.sh
+++ test.sh
@@ -9,13 +9,12 @@ arr=(`echo $text`)
 ssh ${USER}@${HOST} "docker exec ${arr[0]} /usr/bin/mysqldump -u root -p\"${MSPASSWD}\" -x --all-databases > dump.sql"
 ssh ${USER}@${HOST} "mv dump.sql /data_mysql/" 
 
-ssh ${USER}@${HOST} "cd /home/khiraiwa/compose-myserver"
-ssh ${USER}@${HOST} "docker-compose stop"
-ssh ${USER}@${HOST} "docker-compose up -d"
+ssh ${USER}@${HOST} "cd /home/khiraiwa/compose-myserver;docker-compose stop"
+ssh ${USER}@${HOST} "cd /home/khiraiwa/compose-myserver;docker-compose up -d"
 
 ssh ${USER}@${HOST} "docker exec ${arr[0]} /usr/bin/mysqladmin -u root password \"${MSPASSWD}\""
 ssh ${USER}@${HOST} "docker exec ${arr[0]} mysql -u root -p\"${MSPASSWD}\" < /data_mysql/dump.sql"
 
-ssh ${USER}@${HOST} "docker-compose stop"
-ssh ${USER}@${HOST} "docker-compose start"
+ssh ${USER}@${HOST} "cd /home/khiraiwa/compose-myserver;docker-compose stop"
+ssh ${USER}@${HOST} "cd /home/khiraiwa/compose-myserver;docker-compose start"
 
