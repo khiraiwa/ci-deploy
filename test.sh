853fc68a88d13dc711d8c22d981894fc7bffb876
khiraiwa <the.world.nova@gmail.com>
2015-05-24 18:20:30 +0900

    [WIP] test

--- test.sh
+++ test.sh
@@ -6,7 +6,7 @@ MSPASSWD=${PASSWD}
 
 text=$(ssh ${USER}@${HOST} "docker ps | grep -e docker-mysql")
 arr=(`echo $text`)
-ssh ${USER}@${HOST} "docker exec -i ${arr[0]} /usr/bin/mysqldump -u root -p\"${MSPASSWD}\" -x --all-databases > dump.sql"
+ssh ${USER}@${HOST} "docker exec ${arr[0]} /usr/bin/mysqldump -u root -p\"${MSPASSWD}\" -x --all-databases > dump.sql"
 ssh ${USER}@${HOST} "mv dump.sql /data_mysql/" 
 
 ssh ${USER}@${HOST} "cd /home/khiraiwa/compose-myserver;docker-compose stop"
@@ -15,7 +15,7 @@ ssh ${USER}@${HOST} "cd /home/khiraiwa/compose-myserver;docker-compose up -d"
 text=$(ssh ${USER}@${HOST} "docker ps | grep -e docker-mysql")
 arr=(`echo $text`)
 
-ssh ${USER}@${HOST} "docker exec -i ${arr[0]} /usr/bin/mysqladmin -u root password \"${MSPASSWD}\""
+ssh ${USER}@${HOST} "docker exec ${arr[0]} /usr/bin/mysqladmin -u root password \"${MSPASSWD}\""
 ssh ${USER}@${HOST} "docker exec -i ${arr[0]} mysql -u root -p\"${MSPASSWD}\" < /data_mysql/dump.sql"
 
 ssh ${USER}@${HOST} "cd /home/khiraiwa/compose-myserver;docker-compose stop;docker-compose start"
