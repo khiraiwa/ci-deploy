009043f46ad8050bf2c680067405e1ad0228431b
khiraiwa <the.world.nova@gmail.com>
2015-05-24 16:17:32 +0900

    [WIP] test

--- test.sh
+++ test.sh
@@ -2,11 +2,9 @@
 
 USER=${SERVER_USER}
 HOST=${SERVER_HOST}
-PASSWD=${PASSWD}
+MSPASSWD=${PASSWD}
 
-echo ${USER}
-echo ${HOST}
-echo ${PASSWD}
+text=$(ssh ${USER}@${HOST} "docker ps | grep -e docker-mysql")
+arr=(`echo $text`)
+docker exec ${arr[0]} mysqldump -u root p ${MSPASSWD} -x --all-databases > dump.sql
 
-#text=$(ssh ${USER}@${HOST} "docker ps | grep -e docker-mysql")
-#docker exec ${arr[0]} mysqldump -u root p ${MSQL_PSWD} -x --all-databases > dump.sql
