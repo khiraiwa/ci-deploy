888659150601e0c97182e6db62ebfdfe035f6db1
khiraiwa <the.world.nova@gmail.com>
2015-05-24 17:04:58 +0900

    [WIP] test

--- test.sh
+++ test.sh
@@ -6,6 +6,16 @@ MSPASSWD=${PASSWD}
 
 text=$(ssh ${USER}@${HOST} "docker ps | grep -e docker-mysql")
 arr=(`echo $text`)
-ssh ${USER}@${HOST} "docker exec ${arr[0]} /usr/bin/mysqldump -u root -p\"ME09rebR\" -x --all-databases > dump.sql"
+ssh ${USER}@${HOST} "docker exec ${arr[0]} /usr/bin/mysqldump -u root -p\"${MSPASSWD}\" -x --all-databases > dump.sql"
 ssh ${USER}@${HOST} "mv dump.sql /data_mysql/" 
+
+ssh ${USER}@${HOST} "cd /home/khiraiwa/compose-myserver"
+ssh ${USER}@${HOST} "docker-compose stop"
+ssh ${USER}@${HOST} "docker-compose up -d"
+
+ssh ${USER}@${HOST} "docker exec ${arr[0]} /usr/bin/mysqladmin -u root password \"${MSPASSWD}\""
+ssh ${USER}@${HOST} "docker exec ${arr[0]} mysql -u root -p\"${MSPASSWD}\" < /data_mysql/dump.sql"
+
+ssh ${USER}@${HOST} "docker-compose stop"
+ssh ${USER}@${HOST} "docker-compose start"
 
