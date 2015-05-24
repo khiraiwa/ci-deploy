65385efb4836f0d4a60c8df8b507b16aa36925fe
khiraiwa <the.world.nova@gmail.com>
2015-05-24 18:29:02 +0900

    [WIP] test

--- test.sh
+++ test.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+USER=${SERVER_USER}
+HOST=${SERVER_HOST}
+MSPASSWD=${PASSWD}
+
 text=$(ssh ${USER}@${HOST} "docker ps | grep -e docker-mysql")
 arr=(`echo $text`)
 ssh ${USER}@${HOST} "docker exec ${arr[0]} /usr/bin/mysqldump -u root -p\"${MSPASSWD}\" -x --all-databases > dump.sql"
@@ -13,6 +17,7 @@ arr=(`echo $text`)
 
 ssh ${USER}@${HOST} "docker exec ${arr[0]} /usr/bin/mysqladmin -u root password \"${MSPASSWD}\""
 ssh ${USER}@${HOST} "docker exec -i ${arr[0]} mysql -u root -p\"${MSPASSWD}\" < /data_mysql/dump.sql"
+
 ssh ${USER}@${HOST} "cd /home/khiraiwa/compose-myserver;docker-compose stop;docker-compose start"
 
 
